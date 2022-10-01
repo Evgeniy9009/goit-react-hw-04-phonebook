dedae8b15048122bbee51117c27921fcd0ab5c68
Evgeniy <evgeniy.grabar1@gmail.com>
2022-10-01 22:24:42 +0300

    add start files

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import Contacts from "./Contacts/Contacts";
+
+export const App = () => {
+  return (
+    <div
+      style={{
+
+        display: 'flex',
+        justifyContent: 'center',
+        // alignItems: 'center',
+        // fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <Contacts/>
+    </div>
+  );
+};
