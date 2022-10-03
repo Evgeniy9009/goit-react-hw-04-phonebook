7a2a7f97b504a4134cb79525087751eedb81514c
Evgeniy <evgeniy.grabar1@gmail.com>
2022-10-03 21:57:07 +0300

    add

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,84 +1,92 @@
-import { Component } from 'react'
 import ContactForm from 'components/ContactForm/ContactForm'
 import ContactList from 'components/ContactList/ContactList'
 import Filter from 'components/Filter/Filter'
 import { nanoid } from 'nanoid'
+import { useEffect, useState } from 'react'
 
-export default class Contacts extends Component {
+export default function Contacts() {
 
-    state = {
-        contacts: [
+    const startContact = [
     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
-        filter: ''
-    }
+  ]
 
-    componentDidMount() {
-        console.log("componentDidMount")
-        const contacts = JSON.parse(localStorage.getItem("contacts"))
-        if (contacts?.length) {
-            console.log(contacts)
-            this.setState({
-                contacts
-            })
+    const [contacts, setContacts] = useState(() => {
+        const value = JSON.parse(localStorage.getItem("contacts"))
+        return value ?? startContact
+    })
+    const [filter, setFilter] = useState("")
+
+    useEffect(() => {
+        localStorage.setItem("contacts", JSON.stringify(contacts))
+    }, [contacts])
+
+    useEffect(() => {
+        return () => {
+            localStorage.removeItem("contacts")
         }
-    } 
+    }, [])
+    // componentDidMount() {
+    //     console.log("componentDidMount")
+    //     const contacts = JSON.parse(localStorage.getItem("contacts"))
+    //     if (contacts?.length) {
+    //         console.log(contacts)
+    //         this.setState({
+    //             contacts
+    //         })
+    //     }
+    // } 
     
-    componentDidUpdate(prevProps, prevState) {
-        console.log("componentDidUpdate")
-        const { contacts } = this.state
-        if (prevState.contacts !== contacts) {
-            localStorage.setItem("contacts", JSON.stringify(contacts))
-        }
-    }
+    // componentDidUpdate(prevProps, prevState) {
+    //     console.log("componentDidUpdate")
+    //     const { contacts } = this.state
+    //     if (prevState.contacts !== contacts) {
+    //         localStorage.setItem("contacts", JSON.stringify(contacts))
+    //     }
+    // }
 
-    componentWillUnmount() {
-        console.log("componentWillUnmount")
-        // localStorage.removeItem('contacts')
-    }
+    // componentWillUnmount() {
+    //     console.log("componentWillUnmount")
+    //     // localStorage.removeItem('contacts')
+    // }
 
-    addContact = (contact) => {
-        if (this.isDuplicate(contact)) {
-            return alert (`${contact.name} is already in contacts.`)
-        }
-        this.setState((prev) => {
+    const addContact = (contact) => {
+        // if (isDuplicate(contact)) {
+        //     return alert (`${contact.name} or ${contact.number} is already in contacts.`)
+        // }
+        setContacts((prev) => {
             const newContact = {
                 id : nanoid(),
                 ...contact
             }
-            return {
-                contacts: [...prev.contacts, newContact]
-            }
+            console.log(prev)
+            console.log(newContact)
+            return [...prev, newContact]
+            
         })
     }
 
-    removeContact = (id) => {
-        this.setState((prev) => {
-            const newContacts = prev.contacts.filter((item) => item.id !== id)
-            return {
-                contacts: newContacts
-            }
+    const removeContact = (id) => {
+        setContacts((prev) => {
+            const newContacts = prev.filter((item) => item.id !== id)
+            return newContacts
+            
         } )
     }
 
-    isDuplicate({ name, number }) {
-        const { contacts } = this.state
-        const res = contacts.find((item) => item.name.toLocaleLowerCase() === name.toLocaleLowerCase() || item.number === number)
-        return res
+    // const isDuplicate = ({ name, number }) => {
+    //     const res = contacts.find((item) => item.name.toLocaleLowerCase() === name.toLocaleLowerCase() || item.number === number)
+    //     return res
+    // }
+
+    const handleChange = (e) => {
+        const {value } = e.target;
+        setFilter(value)
     }
 
-    handleChange = (e) => {
-        const { name, value } = e.target;
-        this.setState({
-            [name]: value
-        })
-    }
-
-    getFilteredContact() {
-        const { contacts, filter } = this.state
+    const getFilteredContact = () => {
         if (!filter) {
             return contacts
         }
@@ -93,26 +101,16 @@ export default class Contacts extends Component {
         return filteredContacts
     }
 
+    const contactsFiltered = getFilteredContact()
 
-
-    render() {
-        const {addContact, handleChange, removeContact} = this
-        const { filter } = this.state
-        const contacts = this.getFilteredContact()
-        
-        return (
+  return (
             <div>
                 <h1>Phonebook</h1>
                 <ContactForm onSubmit={addContact} />
                 
                 <h2>Contacts</h2>
                 <Filter filter={filter} handleChange={handleChange} />
-                <ContactList items={contacts} removeContact={removeContact} />
+                <ContactList items={contactsFiltered} removeContact={removeContact} />
             </div>
     )
-    }
 }
-
-
-
-
